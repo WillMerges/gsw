1a6df91fcc12cabcd31bab2060982d64c1793df0
WillMerges <wdmerges@gmail.com>
2023-04-20 22:13:01 -0700

    added triple buffer

---
+++ lib/triple_buffer/src/test.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#include "lib/triple_buffer/TripleBuffer.h"
+
+int main() {
+    TripleBuffer<int> buff{};
+
+    int* w;
+    int* r;
+
+    w = buff.write();
+    *w = 5;
+    w = buff.write();
+
+    r = buff.read();
+    if(*r != 5) {
+        printf("failed unit test :(\n");
+    }
+}
